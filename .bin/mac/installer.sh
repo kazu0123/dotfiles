7c027cef0c96ec79bc9d5a8dcd2633b46182f2ef
kazu0123 <kokubo.kazuki01.23@gmail.com>
2024-11-18 01:03:33 +0900

    add .gitconfig

--- .bin/mac/installer.sh
+++ .bin/mac/installer.sh
@@ -2,3 +2,4 @@
 
 ln -s ~/dotfiles/.config/AquaSKK/keymap.conf ~/Library/Application\ Support/AquaSKK/keymap.conf
 ln -s ~/dotfiles/.config/zsh/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.config/git/.gitconfig ~/.gitconfig

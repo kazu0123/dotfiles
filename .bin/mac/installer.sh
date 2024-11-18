5be48f230ba1eb5275961cb265a9c4eb7ea0f12c
kazu0123 <kokubo.kazuki01.23@gmail.com>
2024-11-18 19:48:57 +0900

    add vscode settings

--- .bin/mac/installer.sh
+++ .bin/mac/installer.sh
@@ -6,3 +6,6 @@ ln -s ~/dotfiles/.config/git/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.config/gnupg/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 ln -s ~/dotfiles/.config/gnupg/.gnupg/sshcontrol ~/.gnupg/sshcontrol
 ln -s ~/dotfiles/.config/zsh/.zshrc ~/.zshrc
+ln -s /Users/kazuki/dotfiles/.config/VSCode/Code/User/settings.json /Users/kazuki/Library/Application\ Support/Code/User/settings.json
+ln -s /Users/kazuki/dotfiles/.config/VSCode/Code/User/keybindings.json '/Users/kazuki/Library/Application Support/Code/User/keybindings.json'
+ln -s /Users/kazuki/dotfiles/.config/VSCode/Code/User/tasks.json '/Users/kazuki/Library/Application Support/Code/User/tasks.json'

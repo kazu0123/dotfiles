fb9bab6c52977b4d7136e0e936a506ec803e6815
kazu0123 <kokubo.kazuki01.23@gmail.com>
2024-11-18 14:18:31 +0900

    add gpg

--- .bin/mac/installer.sh
+++ .bin/mac/installer.sh
@@ -3,3 +3,6 @@
 ln -s ~/dotfiles/.config/AquaSKK/keymap.conf ~/Library/Application\ Support/AquaSKK/keymap.conf
 ln -s ~/dotfiles/.config/zsh/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.config/git/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.config/gnupg/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+ln -s ~/dotfiles/.config/gnupg/.gnupg/sshcontrol ~/.gnupg/sshcontrol
+ln -s ~/dotfiles/.config/zsh/.zshrc ~/.zshrc

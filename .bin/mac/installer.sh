6138f95b3689ed38bce3768b87cd65e8bb92c4ab
kazu0123 <kokubo.kazuki01.23@gmail.com>
2024-11-19 12:02:22 +0900

    remove aquaskk

--- .bin/mac/installer.sh
+++ .bin/mac/installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ln -s ~/dotfiles/.config/AquaSKK/keymap.conf ~/Library/Application\ Support/AquaSKK/keymap.conf
+#ln -s ~/dotfiles/.config/AquaSKK/keymap.conf ~/Library/Application\ Support/AquaSKK/keymap.conf
 ln -s ~/dotfiles/.config/zsh/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.config/git/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.config/gnupg/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf

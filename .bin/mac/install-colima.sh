622dc7f395cbaf4e66a10d77fec818d152f67f29
kazu0123 <kokubo.kazuki01.23@gmail.com>
2024-11-19 12:15:44 +0900

    update vm settings

--- .bin/mac/install-colima.sh
+++ .bin/mac/install-colima.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 brew install docker lima colima docker-compose
-colima start --cpu 8 --memory 96 --disk 120
+colima start --cpu 8 --memory 96 --disk 120 --vm-type vz --vz-rosetta --mount-type virtiofs default
 docker context use colima
 mkdir ~/.docker/cli-plugins/
 sudo ln /opt/homebrew/bin/docker-compose ~/.docker/cli-plugins/docker-compose

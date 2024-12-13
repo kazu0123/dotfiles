ce07926d7848f893b1ce9f304448b9f6ed4a05f4
kazu0123 <kokubo.kazuki01.23@gmail.com>
2024-12-14 00:54:00 +0900

    add termux

---
+++ .config/termux/.shortcuts/obsidian-pull.sh
@@ -0,0 +1,3 @@
+cd ~/storage/documents/Obsidian/my-obsidian-vault
+git fetch origin
+git pull origin main
---
+++ .config/termux/.shortcuts/obsidian-push.sh
@@ -0,0 +1,27 @@
+YEAR=$(date +%Y)
+MONTH=$(date +%m)
+DAY=$(date +%d)
+
+HOUR=$(date +%H)
+MINUTE=$(date +%M)
+SECOND=$(date +%S)
+
+# YYYY-MM-DD HH:mm:ss
+DATETIME="$(echo $YEAR-$MONTH-$DAY $HOUR:$MINUTE:$SECOND)"
+
+# vault backup: {{hostname}}: {{date}}
+COMMIT_MESSAGE="vault backup from Pixel 8 at ${DATETIME}"
+
+RESULT=$(termux-dialog text -t "Enter commit message: " -i ${COMMIT_MESSAGE})
+
+DIALOG_CODE=$(echo ${RESULT} | jq '.code')
+
+if [ "${DIALOG_CODE}" = "-2" ]; then
+  COMMIT_MESSAGE=$(echo ${RESULT} | jq '.text')
+else
+
+cd ~/storage/documents/Obsidian/my-obsidian-vault
+
+git add.
+git commit -m "${COMMIT_MESSAGE}"
+git push origin main

5ad139394cb1798a48576f93be6aa54300a5fd25
kazu0123 <kokubo.kazuki01.23@gmail.com>
2024-12-19 23:47:25 +0900

    add .NET SDK and Microsoft Power Platform CLI

---
+++ .bin/mac/install-dotnet.sh
@@ -0,0 +1,4 @@
+brew tap isen-ng/dotnet-sdk-versions
+brew install --cask isen-ng/dotnet-sdk-versions/dotnet-sdk8
+
+dotnet tool install --global Microsoft.PowerApps.CLI.Tool
--- .config/zsh/.zprofile
+++ .config/zsh/.zprofile
@@ -1,1 +1,4 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Add .NET Core SDK tools
+export PATH="$PATH:/Users/kazuki/.dotnet/tools"

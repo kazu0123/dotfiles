6303d31555de2793843a5cd7e074582ca2014ce5
kazu0123 <kokubo.kazuki01.23@gmail.com>
2025-01-19 08:11:54 +0900

    add lms download script

---
+++ apps/LM Studio/scripts/macos/download-model.sh
@@ -0,0 +1,6 @@
+lms get lmstudio-community/Llama-3.3-70B-Instruct-GGUF
+lms get lmstudio-community/QwQ-32B-Preview-GGUF
+lms get mlx-community/Llama-3.2-90B-Vision-Instruct-4bit
+lms get Qwen/Qwen2.5-Coder-32B-Instruct-GGUF
+lms get leafspark/Llama-3.2-11B-Vision-Instruct-GGUF
+lms get second-state/StarCoder2-7B-GGUF

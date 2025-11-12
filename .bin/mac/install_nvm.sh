226b7ed05c87883be4943e8492e4620e1747fc45
kazu0123 <kokubo.kazuki01.23@gmail.com>
2025-11-12 23:21:19 +0900

    backup

--- .bin/mac/install_nvm.sh
+++ .bin/mac/install_nvm.sh
@@ -2,8 +2,3 @@
 
 LATEST_NVM_VERSION=$(curl -o- https://api.github.com/repos/nvm-sh/nvm/releases | jq -r '.[0].tag_name')
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_NVM_VERSION/install.sh | bash
-
-
-nvm install 20
-nvm use 20
-npm install -g @google/gemini-cli
